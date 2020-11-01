9c93459f62d4775a761bef4a9eb3c114c5ec0dec
armandsb1 <52469305+armandsb1@users.noreply.github.com>
2020-11-01 21:56:08 +0200

    Update purecloud.js

--- static/js/purecloud.js
+++ static/js/purecloud.js
@@ -4,7 +4,7 @@
 const clientId = '89f29367-dc0e-4fd0-9f55-7175a73ee600';
 const redirectUrl = 'https://purecloudgdprrequests.github.io/index.html';
 //const redirectUrl = 'http://localhost:8080/index.html';
-const environment = 'mypurecloud.ie';
+const environment = 'mypurecloud.de';
 
 const platformClient = require('platformClient');
 const client = platformClient.ApiClient.instance
